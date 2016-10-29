295af2c43034c9851084727602948bada2222513
violenti <rodavidfernandez@gmail.com>
2016-10-29 20:52:51 -0300

    fix query

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def base():
 
     pawsdb= dbapi.connect("paws.dat") #init db
     cursor= pawsdb.cursor()
-    cursor.execute("""create table if not exists water(level text, date text, petId text)""")
+    cursor.execute("""create table if not exists water(value text, date text, petId text)""")
     return 0
 
 base()
@@ -19,9 +19,9 @@ while base() == 0:
     pawsdb= dbapi.connect("paws.dat") #init db
     cursor= pawsdb.cursor()
     valu= water(0) #import water from census, create test diff to 0
-    nivel=str(valu)
+    value=str(valu)
     Date=datetime.now().isoformat()
     Pet='56596fe86154f0f0746ca456' #this value is hardcode, have to get of a table
-    cursor.execute('''insert  into water (level) values(?)''',(nivel,),(date) values(?)''',(Date,),(petId) values(?)''',(Pet,) )
+    cursor.execute('''insert  into water (level) values(?),(value,),(date) values(?),(Date,),(petId) values(?),(Pet,)''')
     pawsdb.commit()
     time.sleep(60)#stop 60 seconds
